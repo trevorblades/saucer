5465d0eff0ea034230a55998856a12d37baf5b3f
Trevor Blades <tdblades@gmail.com>
2019-11-29 13:17:01 -0800

    Add comments

--- client/src/components/instances-table/new-status.js
+++ client/src/components/instances-table/new-status.js
@@ -5,6 +5,7 @@ import {gql, useQuery} from '@apollo/client';
 
 const GET_INSTANCE = gql`
   query GetInstance($id: ID!) {
+    # only need id and status for Apollo to update the cache automatically
     instance(id: $id) {
       id
       status
@@ -13,7 +14,7 @@ const GET_INSTANCE = gql`
 `;
 
 export default function NewStatus(props) {
-  // poll for status updates when the status is 'new'
+  // poll for status updates when the instance is starting
   useQuery(GET_INSTANCE, {
     variables: props.variables,
     pollInterval: 5000
