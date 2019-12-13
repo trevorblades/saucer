016d875a6d87719640c62c3008267145051dbb82
Trevor Blades <tdblades@gmail.com>
2019-12-13 01:06:18 -0800

    Cancel subscriptions associated with a card when the card is removed

--- functions/graphql/resolvers/delete-card.js
+++ functions/graphql/resolvers/delete-card.js
@@ -10,7 +10,16 @@ module.exports = async function deleteCard(parent, args, {user, stripe}) {
     args.id
   );
 
-  // TODO: cancel subscriptions
+  const subscriptions = await stripe.subscriptions.list({
+    customer: user.data.customerId
+  });
+
+  await Promise.all(
+    subscriptions
+      .filter(subscription => subscription.default_source === source.id)
+      .map(subscription => stripe.subscriptions.del(subscription.id))
+  );
+
   // TODO: stop instances
 
   return source.id;
