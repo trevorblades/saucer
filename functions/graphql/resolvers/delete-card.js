520f68af2abf7f16f7a9c5e5574f2ce5e3e72367
Trevor Blades <tdblades@gmail.com>
2019-12-13 14:42:25 -0800

    Stop instances when a card is deleted

--- functions/graphql/resolvers/delete-card.js
+++ functions/graphql/resolvers/delete-card.js
@@ -1,4 +1,5 @@
 const {AuthenticationError} = require('apollo-server-lambda');
+const {EC2} = require('aws-sdk');
 
 module.exports = async function deleteCard(parent, args, {user, stripe}) {
   if (!user) {
@@ -10,17 +11,21 @@ module.exports = async function deleteCard(parent, args, {user, stripe}) {
     args.id
   );
 
-  const subscriptions = await stripe.subscriptions.list({
+  const {data} = await stripe.subscriptions.list({
     customer: user.data.customerId
   });
 
-  await Promise.all(
-    subscriptions
-      .filter(subscription => subscription.default_source === source.id)
-      .map(subscription => stripe.subscriptions.del(subscription.id))
-  );
+  const ec2 = new EC2();
+  const promises = data
+    .filter(subscription => subscription.default_source === source.id)
+    .flatMap(subscription => [
+      // cancel the subscription and stop associated instances
+      stripe.subscriptions.del(subscription.id),
+      ec2
+        .stopInstances({InstanceIds: [subscription.metadata.instance_id]})
+        .promise()
+    ]);
 
-  // TODO: stop instances
-
+  await Promise.all(promises);
   return source.id;
 };
