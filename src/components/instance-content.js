de9c067153407b199aa8cac0ede45525c343e227
Trevor Blades <tdblades@gmail.com>
2019-12-22 18:10:08 -0500

    Tweak instance page layout

--- src/components/instance-content.js
+++ src/components/instance-content.js
@@ -1,8 +1,14 @@
 import EmptyState from './empty-state';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Fragment} from 'react';
 import waiting from '../assets/waiting.png';
-import {Box, LinearProgress, Link, Typography} from '@material-ui/core';
+import {
+  Box,
+  CardActionArea,
+  LinearProgress,
+  Link,
+  Typography
+} from '@material-ui/core';
 import {FiArrowUpRight} from 'react-icons/fi';
 
 export default function InstanceContent(props) {
@@ -24,20 +30,38 @@ export default function InstanceContent(props) {
       );
     case 'Success':
       return (
-        <Link
-          variant="h6"
-          href={`https://${props.instance.name}.saucer.dev/wp-admin`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wordpress admin
-          <Box
-            component={FiArrowUpRight}
-            ml={0.5}
-            size="1em"
-            style={{verticalAlign: -1}}
-          />
-        </Link>
+        <Fragment>
+          <Box my={3} border={1} borderColor="divider" p={2} textAlign="center">
+            <Typography variant="subtitle1">
+              Wordpress is running at{' '}
+              <Link
+                href={`https://${props.instance.name}.saucer.dev/wp-admin`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {`${props.instance.name}.saucer.dev`}
+                <Box
+                  component={FiArrowUpRight}
+                  ml={0.5}
+                  size="1em"
+                  style={{verticalAlign: -1}}
+                />
+              </Link>
+            </Typography>
+          </Box>
+          <Box display="flex">
+            <Box bgcolor="blueviolet" color="white">
+              <CardActionArea>
+                <Box p={4}>
+                  <Typography variant="subtitle1">Guide</Typography>
+                  <Typography variant="h5">
+                    Create a static site with Gatsby
+                  </Typography>
+                </Box>
+              </CardActionArea>
+            </Box>
+          </Box>
+        </Fragment>
       );
     default:
       return null;
