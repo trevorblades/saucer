06b0611c22e581af749a6559833e7994c4f1d44c
Trevor Blades <tdblades@gmail.com>
2020-01-08 21:16:15 -0800

    Add syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        gatsbyRemarkPlugins: ['gatsby-remark-prismjs'],
         defaultLayouts: {
           default: require.resolve('./src/components/mdx-layout.js')
         }
