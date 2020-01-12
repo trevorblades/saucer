7f81adcc2598123b2fffa67d2635ca3e90ab0be1
Trevor Blades <tdblades@gmail.com>
2020-01-12 15:24:19 -0800

    Add 'Add payment plan' button

--- src/components/instance-details/trial-details.js
+++ src/components/instance-details/trial-details.js
@@ -1,28 +1,19 @@
 import PropTypes from 'prop-types';
 import React, {Fragment} from 'react';
-import {Tooltip, Typography} from '@material-ui/core';
-import {formatDistanceToNow} from 'date-fns';
+import {Button, Typography} from '@material-ui/core';
 
 export default function TrialDetails(props) {
   return (
     <Fragment>
-      <Typography>Free trial</Typography>
-      <Typography>
-        {Date.now() > props.expiryDate ? (
-          'Expired'
-        ) : (
-          <Fragment>
-            Expires{' '}
-            <Tooltip title={props.expiryDate.toLocaleString()}>
-              <span>
-                {formatDistanceToNow(props.expiryDate, {
-                  addSuffix: true
-                })}
-              </span>
-            </Tooltip>
-          </Fragment>
-        )}
+      <Typography gutterBottom>
+        Free trial &bull;{' '}
+        {Date.now() > props.expiryDate
+          ? 'Expired'
+          : `Expires ${props.expiryDate.toLocaleString()}`}
       </Typography>
+      <Button variant="contained" color="primary">
+        Add payment plan
+      </Button>
     </Fragment>
   );
 }
