211f1b957e9f5b33a6b3d290d3a46684f77c0d84
Trevor Blades <tdblades@gmail.com>
2020-01-14 17:13:54 -0800

    Add graphql icon

--- src/components/instance-details/copy-url.js
+++ src/components/instance-details/copy-url.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, {useRef, useState} from 'react';
 import copy from 'clipboard-copy';
 import {Box, Chip, Typography} from '@material-ui/core';
+import {ReactComponent as GraphQL} from '../../assets/graphql.svg';
 
 export default function CopyUrl(props) {
   const timeout = useRef(null);
@@ -18,7 +19,8 @@ export default function CopyUrl(props) {
 
   return (
     <Box mb={2} display="flex" alignItems="center">
-      <Box mr={2}>
+      <GraphQL height={24} fill="#e10098" />
+      <Box mx={1}>
         <Typography>{props.url}</Typography>
       </Box>
       <Chip
